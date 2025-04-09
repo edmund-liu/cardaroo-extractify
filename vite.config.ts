788592913dbc3ebaebca36151472b90c73440d51
simal <simalchaudhari20496@gmail.com>
2025-04-09 18:36:33 +0800

    Added changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import dotenv from 'dotenv';
 import { componentTagger } from "lovable-tagger";
+import fs from "fs";
+
+dotenv.config();
+const HTTPS = false;
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    // port: 8080,
-    port: 80,
+    port: HTTPS ? 443 : 80,
+    https: HTTPS
+      ? {
+        key: fs.readFileSync("private.key", "utf8"),
+        cert: fs.readFileSync("certificate.crt", "utf8"),
+        }
+      : undefined, // Use undefined instead of false when not using HTTPS
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
